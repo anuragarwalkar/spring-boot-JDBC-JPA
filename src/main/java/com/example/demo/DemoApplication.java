6b2f589575d54ab7263f0263dbe9833a33374d43
Anurag <anuragarwalkar@gmail.com>
2021-01-27 13:35:16 +0530

    updated application

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.anuragcreation7.com.anuragcreation;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnuragcreationApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnuragcreationApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.anuragcreation7.com.anuragcreation;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AnuragcreationApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
