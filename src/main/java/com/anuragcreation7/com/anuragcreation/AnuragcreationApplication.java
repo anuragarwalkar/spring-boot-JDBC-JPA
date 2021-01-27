269bd112e2bfab7176e93c44052b81e9cca5e76c
Anurag <anuragarwalkar@gmail.com>
2021-01-27 13:06:16 +0530

    first commit

---
+++ src/main/java/com/anuragcreation7/com/anuragcreation/AnuragcreationApplication.java
@@ -0,0 +1,13 @@
+package com.anuragcreation7.com.anuragcreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnuragcreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnuragcreationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuragcreation7/com/anuragcreation/AnuragcreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuragcreation7.com.anuragcreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnuragcreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
